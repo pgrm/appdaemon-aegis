b849349e9945b74c11668e30e7a6eb9d4b9a2db4
Peter Grman <peter.grman@gmail.com>
2025-09-14 00:47:22 +0200

    a bit of clean up

--- src/appdaemon_aegis/__init__.py
+++ src/appdaemon_aegis/__init__.py
@@ -1,4 +1,1 @@
-# SPDX-FileCopyrightText: 2024-present Jules <jules@example.com>
-#
-# SPDX-License-Identifier: MIT
 __version__ = "0.1.0"
--- src/appdaemon_aegis/app.py
+++ src/appdaemon_aegis/app.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: 2024-present Jules <jules@example.com>
-#
-# SPDX-License-Identifier: MIT
 from __future__ import annotations
 
 from appdaemon.plugins.hass.hassapi import Hass
