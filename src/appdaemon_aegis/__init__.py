144529b82a1b53487350b947ca3a6722492814b1
Peter Grman <peter.grman@gmail.com>
2025-09-14 13:01:55 +0200

    fix format errors

--- src/appdaemon_aegis/__init__.py
+++ src/appdaemon_aegis/__init__.py
@@ -1,12 +1,13 @@
 from importlib.metadata import PackageNotFoundError, version as _version
 
 
-
 def __getattr__(name: str):
     if name == "AegisApp":
         from .app import AegisApp as _AegisApp
+
         return _AegisApp
     raise AttributeError(name)
+
 
 try:
     __version__ = _version("appdaemon-aegis")
